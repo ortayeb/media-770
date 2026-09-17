ed5f5bb9ec39a5d289177dd8ae09a0b953582ca4
ortayeb <ortayeb111@gmail.com>
2026-09-17 18:52:18 +0300

    Add main layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "MEDIA 770",
+  description: "אנחנו הופכים עסקים לחוויות דיגיטליות.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="he" dir="rtl">
+      <body>{children}</body>
+    </html>
+  );
+}
